7ad30a027874dc641b27221eb79a5f4bf4af9f1b
EstradaLuisen <estrada.luisen@gmail.com>
2024-07-19 00:10:51 -0500

    ADD ToDo list with API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,105 +1,127 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-//create your first component
 const Home = () => {
-	//Variables to be used
-	const noTask = ["~There is no active tasks~"];	// the first task just to be a placeholder. Will be replaced by the first real task
-	const [tasks, setTasks ] = useState(noTask); // task list
-	const [newTask, setNewTask] = useState("");  //new task as a string
-	const [taskEditDiv, setTaskEditDiv] = useState("") //state to know if the mouse is over a task or not
-	const [info, setInfo] = useState([]);
-    const [data, setData] = useState({
-         title: "",
-         body: "",
-         userId: 1
-    })
+  const apiURL = "https://playground.4geeks.com/todo";
+  const user = "LuisenEstrada"; //the user we are going to use in this practice
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-	// function to know if the user is pressing the "enter" botton in the form input
-	const handleKeyPress = (e) => {
-		if (e.key === 'Enter') {
-		  addAndErase(); //we are doing the add and erase function
-		}
-	};
+  const addNewTask = (newTask) => {
+    if (newTask != "") {
+      sendTasksToApi({ label: newTask, is_done: false });
+    }
+  };
 
-	//Delete a task by its position
+  const keyPressNewUser = (e) => {
+    if (e.key === "Enter") {
+      addNewTask(newTask); //
+    }
+  };
 
-	const deleteTask = (index) => {
-		const taskToDelete = [...tasks];
-		//console.log(index)
-		taskToDelete.splice(index,1);
-		setTasks(taskToDelete);
-	}
+  //Sen NewTask to the API
 
-	// Add and erase function .... add new task to the task list and we erase the current value in the input form 
-	const addAndErase= () => {
-		if (tasks[0] == noTask[0] && newTask !== "") {
-			setTasks([newTask])
-			setNewTask("");
-			//console.log("first task")
-		}
-		else if (newTask != ""){
-			setTasks([...tasks, newTask]);
-			setNewTask("");
-			//console.log("second ore more task")
-		}
-	}
+  const sendTasksToApi = async (task) => {
+    const response = await fetch(`${apiURL}/todos/${user}`, {
+      method: "POST",
+      body: JSON.stringify(task),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      setTasks(tasks.concat(data));
+      setNewTask("");
+    }
+  };
 
-	// Get the info from the API
+  const deleteTask = async (id) => {
+    const response = await fetch(`${apiURL}/todos/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      setTasks(tasks.filter((item) => item.id != id));
+    }
+  };
 
-	 async function getInfo() {
-         try {
-             const response = await fetch(
-                 "https://playground.4geeks.com/todo/docs#/Todo%20operations"
-             );
-             const infoData = await response.json(); // JavaScript Object Notation
-             setInfo(infoData);
-         } catch (e) {
-             console.log(e);
-         }
-     }
+  //Create new user
+  const createUser = async () => {
+    const response = await fetch(`${apiURL}/users/${user}`, {
+      method: "POST",
+    });
+    console.log(response.status);
+  };
 
-     useEffect(() => {
-         getInfo();
-     }, []);
+  //Get user Tasks
+  const getTasks = async (user) => {
+    const response = await fetch(`${apiURL}/users/${user}`);
+    if (response.status == 404) {
+      createUser();
+      return;
+    }
+    if (response.ok) {
+      const data = await response.json();
+      setTasks(data.todos);
+    }
+  };
 
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">My To Do List!</h1>
-			<div className="border border-dark rounded m-auto my-5 w-50 p-3">
-				<div className="input-group  ">
-					<input type="text" className="form-control" placeholder="What needs to be done?" value={newTask} 
-						onChange={(e) => setNewTask(e.target.value)} 
-						onKeyDown={handleKeyPress} //event when the user click the enter button
-					/>
-					<button 
-						className="btn btn-outline-primary" 
-						type="button" 
-						onClick={addAndErase}
-					>
-						Add
-					</button>
-				</div>
-				{tasks.map((task, index) => (
-          			<div className = "d-flex justify-content-center border-top border-dark mt-3 py-2"
-						onMouseEnter={() => setTaskEditDiv(index)}
-						onMouseLeave={() => setTaskEditDiv("")}
-						key={index} >
-						{taskEditDiv === index && tasks[0] != noTask[0] ? 
-							<div className = "bg-primary text-light p-2 rounded border border-light d-flex justify-content-between w-100 h-100">
-								<div className="d-flex align-items-center">{task}</div>
-								<button className="btn btn-outline-light"
-									onClick={()=>deleteTask(index)}> 
-									DONE! 
-								</button>
-							</div>:
-							<div className = "d-flex justify-content-start w-100 h-100">
-							<div className="d-flex align-items-center">{task}</div>
-							</div> }
-					</div>
-        		))}			
-			</div>	
-		</div>
-	);
+  //useEsffect to create new users and get the task list of the user
+  useEffect(() => {
+    getTasks(user);
+  }, []);
+
+  return (
+    <div className="conteiner">
+      <h1 className="text-center my-5">My to do list with API !</h1>
+      <div className="border border-dark rounded m-auto my-3 w-50 p-3">
+        <div className="input-group  ">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="New task"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            onKeyDown={keyPressNewUser} //event when the user click the enter button
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => addNewTask(newTask)}
+          >
+            <div className="text"> Add</div>
+          </button>
+        </div>
+      </div>
+      <div className="border border-dark rounded m-auto my-3 w-50 p-3">
+        {tasks.length != 0 ? (
+          tasks.map((task) => (
+            <div
+              className="d-flex align-items-center justify-content-between"
+              key={task.id}
+            >
+              <div className="w-100 border border-dark p-2 rounded-2 my-2 mx-1">
+                {task.label}
+              </div>
+              <div>
+                <button
+                  className="btn  btn-danger"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  X
+                </button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="d-flex align-items-center justify-content-between">
+            <div className="w-100 d-flex justify-content-center border border-dark p-2 rounded-2 my-2 mx-1">
+              There is no current tasks
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
